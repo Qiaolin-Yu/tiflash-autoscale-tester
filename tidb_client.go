a4dd1bdde155720cb64b02eeeabb9fbb3a51b099
Qiaolin Yu <liin1211@outlook.com>
2023-03-28 19:42:19 +0800

    fix

--- tidb_client.go
+++ tidb_client.go
@@ -128,7 +128,7 @@ func (c *TidbClient) GetTiFlashInformationSchema() []InformationSchema {
 
 func CheckTiFlashReady(schemaRows []InformationSchema) bool {
 	for _, row := range schemaRows {
-		if row.Available <= 1 {
+		if row.Available < 1 {
 			return false
 		}
 	}
